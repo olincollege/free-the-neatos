c93b54524e18cb440d72f294e715e0973ffbc9dc
Andrew Kurtz <akurtz@olin.edu>
2025-12-04 15:57:19 -0500

    Initial ros package

---
+++ freato/freato/go_to_point.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from freato.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ freato/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = "freato"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="akurtz",
+    maintainer_email="akurtz@olin.edu",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    extras_require={
+        "test": [
+            "pytest",
+        ],
+    },
+    entry_points={
+        "console_scripts": ["go_to_point = go_to_point.send_message:main"],
+    },
+)

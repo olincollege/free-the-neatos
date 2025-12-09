68b96f81335cc839b9eece570714081322ba3eac
Jojo Liu <covenanter32@gmail.com>
2025-12-09 00:56:53 -0500

    fixed a reference frame issue that gave bad test results

--- kalman-filter-development/test_ICP.py
+++ kalman-filter-development/test_ICP.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from icp import tf_from_icp as tf_icp
+from icp_class import ICP_2D
 from icp_test_func import Align2D
 
 def test_icp(rotation, translation):
@@ -23,18 +24,26 @@ def test_icp(rotation, translation):
     scan_A_homogeneous = np.hstack([scan_A, np.ones((scan_A.shape[0], 1))])
     scan_B_homogeneous = np.hstack([scan_B, np.ones((scan_B.shape[0], 1))])
 
-    #aligner = Align2D(scan_A_homogeneous, scan_B_homogeneous, np.eye(3))
+    aligner = Align2D(scan_B_homogeneous, scan_A_homogeneous, np.eye(3))
     #T = aligner.AlignICP(50, 1e-6)
 
-    T = tf_icp(scan_B, scan_A, max_iterations=50, tolerance=1e-6)
+    T = tf_icp(scan_B, scan_A, max_iterations=100, tolerance=1e-12)
 
     print("Estimated rotation:\n", T[0:2,0:2])
     print("Estimated translation:\n", T[0:2,2])
-    print("\nExpected rotation:\n", np.array([
+    print("\nExpected rotation:\n", np.linalg.inv(np.array([
         [np.cos(np.deg2rad(rotation)), -np.sin(np.deg2rad(rotation))],
         [np.sin(np.deg2rad(rotation)),  np.cos(np.deg2rad(rotation))]
-    ]))
-    print("Expected translation:\n", translation)
+    ])))
+    print("Expected translation:\n", -1*np.array(translation))
+
+    plt.figure()
+    plt.scatter(scan_A[:,0], scan_A[:,1], s=1, label='Scan A (target)', c='blue')
+    plt.scatter(scan_B[:,0], scan_B[:,1], s=1, label='Scan B (source)', c='red')
+    plt.axis('equal')
+    plt.legend()
+    plt.title("Original Scans")
+    plt.show()
 
 def generate_scan_A():
     """
@@ -51,18 +60,17 @@ def generate_scan_A():
     pts += [[4, y] for y in ys]     # right wall
 
     # circular pillar
-    '''
-    theta = np.linspace(0, 2*np.pi, 80)
-    pts += [[1 + 1.0*np.cos(t), 1 + 1.0*np.sin(t)] for t in theta]
+    # theta = np.linspace(0, 2*np.pi, 80)
+    # pts += [[1 + 1.0*np.cos(t), 1 + 1.0*np.sin(t)] for t in theta]
 
     # block
-    xs = np.linspace(-1.5, -0.5, 40)
-    ys = np.linspace(0.5, 1.5, 40)
-    pts += [[xs[i], 0.5] for i in range(40)]
-    pts += [[xs[i], 1.5] for i in range(40)]
-    pts += [[-1.5, ys[i]] for i in range(40)]
-    pts += [[-0.5, ys[i]] for i in range(40)]
-    '''
+    # xs = np.linspace(-1.5, -0.5, 40)
+    # ys = np.linspace(0.5, 1.5, 40)
+    # pts += [[xs[i], 0.5] for i in range(40)]
+    # pts += [[xs[i], 1.5] for i in range(40)]
+    # pts += [[-1.5, ys[i]] for i in range(40)]
+    # pts += [[-0.5, ys[i]] for i in range(40)]
+    
 
     return np.array(pts)
 
@@ -82,7 +90,7 @@ def generate_scan_B(scan_A, rotation, translation):
     return scan_B
 
 if __name__ == "__main__":
-    test_icp(15, [1, -0.5])
+    test_icp(-5, [0.0, -0.2])
     
     # scan_A = generate_scan_A()
     # scan_B = generate_scan_B(scan_A)

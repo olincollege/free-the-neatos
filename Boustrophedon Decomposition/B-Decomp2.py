232eeb188c02adf8be2d6b4577ef7d21216cfba9
clinehan <lineha.cian28@gmail.com>
2025-12-11 15:30:39 -0500

    Creating B-Decomp folder and code

---
+++ Boustrophedon Decomposition/B-Decomp2.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Sample Occupancy Grid (Comment out and take map from Lidar)
+
+
+
+def b_decomp(map):
+    """
+    Run Boustrophedon Decomposition on Occupancy Map from Lidar
+    will generate path
+        
+    Input:
+        map: occupancy grid (0 is free, 100 is occupied)
+
+    Output:
+        path: path (list of points) to traverse the map, hitting all the area
+    """
+    
+def sample_map():
+    """
+    Generate a sample occupancy grid (map)
+
+    Output:
+        map: occupancy grid (0 is free, 100 is occupied)
+    """
+
+
+
+if __name__ == "__main__":
+    b_decomp()
